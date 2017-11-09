8304c421ffb56e091d454f06cbd2d372d2c81ba2
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-11-09 20:43:16 +0600

    partially created code for creating class Timetable. written constructor for it

--- Prepare Class Routine using Hill Climbing Algorithms/src/Routine/Timetable.java
+++ Prepare Class Routine using Hill Climbing Algorithms/src/Routine/Timetable.java
@@ -1,24 +1,31 @@
-//package Routine;
-//
-//import java.util.List;
-//
-///**
-// * Created by nafee on 11/9/17.
-// */
-//public class Timetable {
-//
-//    private int periodCnt;
-//    private int roomCnt;
-//    private int classCnt;
-//    private int teacherCnt;
-//
-//    private List<Element>[]  schedule;
-//
-//    public Timetable(int periodCnt, int roomCnt, int classCnt, int teacherCnt) {
-//        this.periodCnt = periodCnt;
-//        this.roomCnt = roomCnt;
-//        this.classCnt = classCnt;
-//        this.teacherCnt = teacherCnt;
-//        schedule = new List<Element>[periodCnt+9];
-//    }
-//}
+package Routine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nafee on 11/9/17.
+ */
+public class Timetable {
+
+    private int periodCnt;
+    private int roomCnt;
+    private int classCnt;
+    private int teacherCnt;
+
+    private List<List<Element>>  schedule;
+
+    public Timetable(int periodCnt, int roomCnt, int classCnt, int teacherCnt) {
+        this.periodCnt = periodCnt;
+        this.roomCnt = roomCnt;
+        this.classCnt = classCnt;
+        this.teacherCnt = teacherCnt;
+        schedule = new ArrayList<List<Element>>(periodCnt+9);
+        for (int a = 1; a <= periodCnt; a++ )
+        {
+            schedule.set(a, new ArrayList<Element>());
+        }
+    }
+
+    
+}
