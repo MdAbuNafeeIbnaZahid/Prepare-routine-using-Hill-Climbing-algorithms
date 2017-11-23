fa229646b26a73eeff44c5eeec9005ab699c159a
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-11-23 14:47:05 +0600

    Changed the signature of the method getNextSuccessor. It will now accept an iterable of candidate solutions. NOT just an iterator.

--- Prepare Class Routine using Hill Climbing Algorithms/src/HillClimbing/SelectingSuccessor/SuccessorSelector.java
+++ Prepare Class Routine using Hill Climbing Algorithms/src/HillClimbing/SelectingSuccessor/SuccessorSelector.java
@@ -2,11 +2,10 @@ package HillClimbing.SelectingSuccessor;
 
 import HillClimbing.CandidateSolution;
 
-import java.util.Iterator;
-
 /**
  * Created by nafee on 11/11/17.
  */
+
 public interface SuccessorSelector {
-    public CandidateSolution getNextSuccessor(Iterator<CandidateSolution> iterator, CandidateSolution currentCS );
+    CandidateSolution getNextSuccessor(Iterable<CandidateSolution> candidateSolutions, CandidateSolution currentCS );
 }
