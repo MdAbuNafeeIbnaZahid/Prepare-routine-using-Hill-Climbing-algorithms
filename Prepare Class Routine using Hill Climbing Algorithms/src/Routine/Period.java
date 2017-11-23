d353c7ea83117dde348a6285ce46be34125744a3
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-11-23 11:27:27 +0600

    Implemented a new method for getting conflicting elements from a period.

--- Prepare Class Routine using Hill Climbing Algorithms/src/Routine/Period.java
+++ Prepare Class Routine using Hill Climbing Algorithms/src/Routine/Period.java
@@ -3,9 +3,7 @@ package Routine;
 import Copy.UnoptimizedDeepCopy;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by nafee on 11/10/17.
@@ -13,7 +11,6 @@ import java.util.List;
 public class Period implements Serializable {
 
     private List<Element> elements = new ArrayList<Element>();
-
 
 
     public Period() {
@@ -85,7 +82,27 @@ public class Period implements Serializable {
 
     public Iterator<Element> getConflictingElementIterator()
     {
-        return null;
+        Set<Element> conflictingElements = new HashSet<Element>();
+
+
+        ConflictFinder roomConflictFinder = new RoomConflictFinder();
+        Iterator<Element> roomConflictingElementIterator = roomConflictFinder.getSingleConflictingElementsIterator(elements.iterator());
+
+
+        ConflictFinder classConflictFinder = new ClassConflictFinder();
+        Iterator<Element> classConflictingElementIterator = classConflictFinder.getSingleConflictingElementsIterator(elements.iterator());
+
+        ConflictFinder teacherConflictFinder = new TeacherConflictFinder();
+        Iterator<Element> teacherConflictingElementIterator = teacherConflictFinder.getSingleConflictingElementsIterator(elements.iterator());
+
+
+
+        // forEachRemaining is an interesting feature of java 8
+        roomConflictingElementIterator.forEachRemaining(conflictingElements::add);
+        classConflictingElementIterator.forEachRemaining(conflictingElements::add);
+        teacherConflictingElementIterator.forEachRemaining(conflictingElements::add);
+
+        return conflictingElements.iterator();
     }
 
     @Override
