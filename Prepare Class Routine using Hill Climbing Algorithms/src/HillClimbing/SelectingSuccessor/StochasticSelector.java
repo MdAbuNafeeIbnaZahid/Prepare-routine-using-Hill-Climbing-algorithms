c79df1402b36b029de473cd5374fc2a3a879ddb2
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2017-11-24 13:02:58 +0600

    Added a class for schotastic selecting of neighbors. This class implements the interface SuccessorSelector. Now the class is just a dummy

---
+++ Prepare Class Routine using Hill Climbing Algorithms/src/HillClimbing/SelectingSuccessor/StochasticSelector.java
@@ -0,0 +1,15 @@
+package HillClimbing.SelectingSuccessor;
+
+import HillClimbing.CandidateSolution;
+
+/**
+ * Created by nafee on 11/24/17.
+ */
+public class StochasticSelector implements SuccessorSelector {
+
+    @Override
+    public CandidateSolution getNextSuccessor(Iterable<CandidateSolution> neighbors, CandidateSolution currentCS) {
+        return null;
+    }
+
+}
